ccb368a6fb30a84fafa620270e1102435af51bff
JoLaziz <laziz.youssef@gmail.com>
2026-06-20 00:24:26 +0200

    remove default login and register values

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -47,7 +47,7 @@ function RegisterPage() {
             <UserRound className="absolute left-3 top-1/2 -translate-y-1/2 text-[#6d7a73]" size={17} />
             <input
               className="w-full rounded-lg border border-[#bccac1] bg-[#eff5ef] py-3 pl-10 pr-3 text-sm outline-none transition focus:border-[#00694c] focus:ring-2 focus:ring-[#00694c]"
-              defaultValue="Maya Ben Ali"
+              defaultValue=""
               name="name"
               type="text"
             />
@@ -60,7 +60,7 @@ function RegisterPage() {
             <Mail className="absolute left-3 top-1/2 -translate-y-1/2 text-[#6d7a73]" size={17} />
             <input
               className="w-full rounded-lg border border-[#bccac1] bg-[#eff5ef] py-3 pl-10 pr-3 text-sm outline-none transition focus:border-[#00694c] focus:ring-2 focus:ring-[#00694c]"
-              defaultValue="maya@smarthealth.local"
+              defaultValue=""
               name="email"
               type="email"
             />
